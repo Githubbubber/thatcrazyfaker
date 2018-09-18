1416970027dae6f47600234c7c4c0d46711f55da
Githubbubber <mekesiabrown@gmail.com>
2018-09-17 23:50:21 -0400

    Add Post model to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#goodbye'
+  root 'posts#index'
+
+  resources :posts
 end
+
+# From a rake routes:
+#  					 Prefix Verb   URI Pattern                                                                              Controller#Action
+#                     posts GET    /posts(.:format)                                                                         posts#index
+#                           POST   /posts(.:format)                                                                         posts#create
+#                  new_post GET    /posts/new(.:format)                                                                     posts#new
+#                 edit_post GET    /posts/:id/edit(.:format)                                                                posts#edit
+#                      post GET    /posts/:id(.:format)                                                                     posts#show
+#                           PATCH  /posts/:id(.:format)                                                                     posts#update
+#                           PUT    /posts/:id(.:format)                                                                     posts#update
+#                           DELETE /posts/:id(.:format)                                                                     posts#destroy
+#                      root GET    /                                                                                        posts#index
+#                           GET    /posts(.:format)                                                                         posts#index
+#                           POST   /posts(.:format)                                                                         posts#create
+#                           GET    /posts/new(.:format)                                                                     posts#new
+#                           GET    /posts/:id/edit(.:format)                                                                posts#edit
+#                           GET    /posts/:id(.:format)                                                                     posts#show
+#                           PATCH  /posts/:id(.:format)                                                                     posts#update
+#                           PUT    /posts/:id(.:format)                                                                     posts#update
+#                           DELETE /posts/:id(.:format)                                                                     posts#destroy
+#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
