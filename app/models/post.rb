5bb95dbc81ef79cae598f00b9767ea22d07ece0d
Githubbubber <mekesiabrown@gmail.com>
2018-09-19 20:04:23 -0400

    Update model Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,16 @@ class Post < ApplicationRecord
 	
   validates :title,
       presence: true,
-      length: { minimum: 7, maximum:10 },
+      length: { minimum: 7, maximum: 10 },
       format: { with: /[A-Za-z0-9._]/ }
 
   validates :author,
       presence: true,
-      length: { minimum: 4 },
+      length: { minimum: 4, maximum: 20 },
       format: { with: /[A-Za-z0-9._]/ }
 
   validates :content,
       presence: true,
-      length: { minimum: 8 },
+      length: { minimum: 8, maximum: 50 },
       format: { with: /[\s\w.!\-,:;\']/ }
 end
